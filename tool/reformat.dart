8d5ae9cb9832ad51bd1c3f4857ea8e5f75216d6b
Pascal Welsch <pascal.welsch@gmail.com>
2018-02-12 00:36:11 +0100

    Only print relevant reformatting outputs

--- tool/reformat.dart
+++ tool/reformat.dart
@@ -2,8 +2,21 @@ import 'dart:async';
 import 'dart:io';
 
 Future<Null> main(List<String> args) async {
-  await Process.start('dartfmt', ['-l 120', '-w', 'bin', 'lib', 'test', 'tool']).then((process) {
-    stdout.addStream(process.stdout);
+  Process p = await Process
+      .start('dartfmt', ['--set-exit-if-changed', '-l 120', '-w', 'bin', 'lib', 'test', 'tool']).then((process) {
+    stdout.writeln('Reformatting project with dartfmt');
+
+    var out = process.stdout
+        .map((it) => new String.fromCharCodes(it))
+        .where((it) => !it.contains("Unchanged"))
+        .map((it) => it.replaceFirst('Formatting directory ', ''))
+        .map((it) => it.codeUnits);
+
+    stdout.addStream(out);
     stderr.addStream(process.stderr);
+
+    return process;
   });
+
+  exit(await p.exitCode);
 }
