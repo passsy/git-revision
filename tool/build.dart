c6fe1f14f6c2bafa8c9350d19d2ca201b0a16606
Pascal Welsch <pascal.welsch@gmail.com>
2018-08-10 22:49:55 +0000

    Override generated files

--- tool/build.dart
+++ tool/build.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'dart:io';
 
 Future<Null> main(List<String> args) async {
-  Process p = await Process.start('pub', ['run', 'build_runner', 'build']).then((process) {
+  Process p = await Process.start('pub', ['run', 'build_runner', 'build', '--delete-conflicting-outputs']).then((process) {
     stdout.addStream(process.stdout);
     stderr.addStream(process.stderr);
 
